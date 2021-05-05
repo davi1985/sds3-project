6026021cb128e2366399debda166673ca00e8537
Davi Silva <davisilvaphoto@gmail.com>
2021-05-04 21:50:00 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,19 @@
+import { DataTable } from 'components/DataTable'
+import { Footer } from 'components/Footer'
+import { NavBar } from 'components/NavBar'
+
 export function App() {
-  return <h1 className="text-primary">DevSuperior</h1>
+  return (
+    <>
+      <NavBar />
+
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+
+        <DataTable />
+      </div>
+
+      <Footer />
+    </>
+  )
 }
